2d737fe7f7be3471df242078dbc825d73a8be88f
Viktor Egorov <netfantom.spb@gmail.com>
2021-01-18 20:08:27 +0300

    Update certs

--- nodejs_express_https_ssl/keys/create_certificate_for_domain.sh
+++ nodejs_express_https_ssl/keys/create_certificate_for_domain.sh
@@ -4,4 +4,5 @@ openssl req -new -newkey rsa:2048 -sha256 -nodes \
 # -subj="/CN=127.0.0.1"    
 # Generate Key file and Certificate file
 openssl x509 -req -in ./server/server.csr -CA ./rootCA/rootCA.crt \
-    -CAkey ./rootCA/rootCA.key -CAcreateserial -out ./server/server.crt -days 3650 -sha256 -extfile ./v3.ext
+    -CAkey ./rootCA/rootCA.key -CAcreateserial -out ./server/server.crt -days 3650 -sha256 -extfile ./server.ext
+rm ./server/server.csr
